66006b25d8a0ef365b24d13e21f0814169c7acde
Amandine Lee <amandine.m.lee@gmail.com>
2013-08-07 15:30:32 -0400

    Cleaned up so in basic main function and pure functions format

--- gen_alignment_matrix.py
+++ gen_alignment_matrix.py
@@ -1,23 +1,36 @@
 #import scipy.sparse as ss
 from collections import defaultdict
 
-'''Should I make this all into one giant class? How will it be easiest to test? I guess the question is, what is the end goal.'''
+
 
 '''Output looks like: 
     SentenceIndex EnglishIndex ForeignIndex'''
+'''Let's start with some global variables and pure functions'''
+def main():
+    
+    c_e = defaultdict(float)
+    c_s_given_e = defaultdict(float)
 
-ALIGHMENTS = []
-E_COUNTS = defaultdict(float)
-SE_COUNTS = defaultdict(float)
 
+    english_file = 'corpus.en'
+    spanish_file = 'corpus.es'
+    eng_sents = get_sents(english_file)
+    spa_sents = get_sents(spanish_file)
+    eng_lex = make_lexicon(eng_sents)
+    spa_lex = make_lexicon(spa_sents)
+    length = len(eng_sents)
 
-english_file = 'corpus.en'
-spanish_file = 'corpus.es'
-eng_sents = get_sents(english_file)
-spa_sents = get_sents(spanish_file)
+'''Initialization methods'''
+def get_sents(filename):
+    with open(filename) as f:
+        return f.readlines()
 
-length = len(eng_sents)
-
+def make_lexicon(sentences):
+    lex = set()
+    for sent in sentences:
+        for word in sent.split():
+            lex.add(word)
+    return lex
 #k is the sentence pair index. i is the foreign word index. j is the english word index. Gotta search over every english word and add up the probaiblities of the foreign word being translated from that english word. 
 
 def delta(k, i, j):
@@ -31,39 +44,22 @@ def gen_expected_counts():
     for k in xrange(length):
         for word_i in eng_sents[k].split():
             for word_j in spa_sents[k].split(): #What do we do with the commas? Do we count them?
-                SE_COUNTS[(word_i, word_j)] = E_COUNTS[(i, j)] + delta(k, i, j)
-                E_COUNTS[word_j] = E_COUNTS[word_j] + delta(k, i, j)
+                c_s_given_e[(word_i, word_j)] = c_e[(i, j)] + delta(k, i, j)
+                c_e[word_j] = c_e[word_j] + delta(k, i, j)
 
 def update_t(eng_lex, spa_lex):
     for eword in eng_lex:
         for sword in spa_lex:
-            T[(eword, sword)] = SE_COUNTS(eword, sword)/E_COUNTS(eword)
+            T[(eword, sword)] = c_s_given_e(eword, sword)/c_e(eword)
    #I need to do this for ALL possible foreign and english words? So I probably want to compile all of the words, and then go through both initializing parameters in a giant for loop/dict comprehension
-class Alignments(object):
-    
-    def __init__(self, engfile, spafile):
-        self.eng_sents = self.get_sents(engfile)
-        self.spa_sents = self.get_sents(spafile)
-
-    def get_sents(self, filename):
-        with open(filename) as f:
-            return f.readlines()
 
 
 
 
-def make_lexicon(sentences):
-    lex = set()
-    for sent in sentences:
-        for word in sent.split():
-            lex.add(word)
-    return lex
 
-def initialization_IMB1():
-    self.eng_lex = make_lexicon(eng_lex)
-    self.spa_lex = make_lexicon(spa_les)
 
-#So one option here would be to convert these sets into lists and refer to the words by their indeces.  
+def initialization_IMB1(eng_lex, spa_lex):
+
     for eng in eng_lex:
         for spa in spa_lex:
             T[(spa, eng)] = 1.0/len(eng_lex)
--- test_alignments.py
+++ test_alignments.py
@@ -6,27 +6,34 @@ from gen_alignment_matrix import *
 3. Run on dev data and write spanish word corresponding to the highest t value for each english word as in the sample output
 '''
 
-def test_get_sents():
+class TestEM():
+
+    def test_get_sents(self):
 #Probably should do this with fake files because the given files could change at anytime.
 
-    with file('ex.txt', 'w') as f:
-        for i in xrange(100 ):
-            f.write('bananas apples\n')
+        with file('ex.txt', 'w') as f:
+            for i in xrange(100 ):
+                f.write('bananas apples\n')
 
-    out = get_sents('ex.txt')
+        out = get_sents('ex.txt')
 
-    assert len(out) == 100
-    assert out[1] == 'bananas apples\n'
-    assert out[1].split()[1] == 'apples'
-    assert len(out[1].split()) == 2
+        assert len(out) == 100
+        assert out[1] == 'bananas apples\n'
+        assert out[1].split()[1] == 'apples'
+        assert len(out[1].split()) == 2
 
-def test_make_lexicon():
-    assert len(make_lexicon(get_sents('corpus.en'))) > 5401
+    def test_make_lexicon(self):
+        fake_sentences = ['i have a kiwi .',
+                          'his name name is steve .',
+                          'hacker school has lots of people , and i am one of them .']
+        fake_lex = {'i', 'have', 'a', 'kiwi','.','his','name','is','steve','hacker','school',
+                    'has','lots','of','people','and','am','one','them',','}
 
+        assert make_lexicon(fake_sentences) == fake_lex
 
 '''def test_initialization():
 #Will this take for fucking ever?
-    eng_lex, spa_lex, T = initialization_IMB1()
+    T = initialization_IMB1()
     print len(eng_lex)
     print len(spa_lex)
     comp = T.values()[1]
