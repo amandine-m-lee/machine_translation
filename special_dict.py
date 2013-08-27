f8c2f55a9da1c9b4ac3bf9ac53a5f136ae64c6d9
Amandine Lee <amandine.m.lee@gmail.com>
2013-08-27 17:46:30 -0400

    Added hashes to distinguish between files of different dictionaries

--- special_dict.py
+++ special_dict.py
@@ -4,19 +4,20 @@ class BigDict(object):
     #have a hash that distinguishes files from this specific one? 
     def __init__(self):
         pass
+
     def __getitem__(self, item):
         try:
-            with open('bd_'+item, 'r') as f:
+            with open('.bd_'+item, 'r') as f:
                 return pickle.load(f)
                        
         except IOError:
             raise KeyError
 
     def __setitem__(self, item, value):
-        with open('bd_'+item, 'w') as f:
+        with open('.bd' + repr(hash(self)) + '_' + repr(item), 'w') as f:
             pickle.dump(value, f)
     def __repr__(self):
-        return "<BigDict of type {}>".format(self.dtype)
+        return "<BigDict {}>".format(hash(self))
     def __len__(self):
         with open('bd_' + item, 'r') as f:
             #Other thought: keep this as sa member of the class
