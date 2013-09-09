0380e8ec58831ec22dac95c31f7a5394960c3993
Amandine Lee <amandine.m.lee@gmail.com>
2013-09-08 23:58:23 -0700

    Added a bunch more standard dunder methods

--- special_dict.py
+++ special_dict.py
@@ -4,19 +4,18 @@ class BigDict(object):
     #have a hash that distinguishes files from this specific one? 
     def __init__(self):
         self.all_keys = set()
-        pass
 
     def __getitem__(self, item):
-
+        print "GETTING" + repr(item)
         if item in self.all_keys:
             with open('.bd'+repr(hash(self)) + '_' + str(item), 'r') as f:
                 return pickle.load(f)
-                       
         else:
             raise KeyError
 
     def __setitem__(self, item, value):
         self.all_keys.add(item)
+        print "SETTING" + repr((item, value))
         with open('.bd' + repr(hash(self)) + '_' + str(item), 'w') as f:
             pickle.dump(value, f)
 
@@ -26,8 +25,17 @@ class BigDict(object):
     def __len__(self):
         return len(self.all_keys)
 
+    def __iter__(self):
+        for key in self.all_keys:
+            yield key
+    def __delitem__(self, item):
+        self.all_keys.remove(item) #Won't change contents of the file 
+    def __contains__(self, item):
+        return item in self.all_keys
+
     def keys(self): #Is this a bad idea? Maybe, but I like the idea of replicating
         #All of the functionality of a normal dictionary (except speed of course)
+        #not sure what order things should be in 
         return list(self.all_keys)
 
     def values(self): #Warning: probably horrible runtime
@@ -42,4 +50,8 @@ class BigDict(object):
         for key in self.all_keys:
             itms.append((key, self[key]))
 
-
+    def get(self, item):
+        return self.__getitem__(item)
+#More fun with dunder methods.
+    def __nonzero__(self):
+        return not len(self) == 0
