5934a0a9f34b75d21156d2b2c9b8f76d68d83a3d
Amandine Lee <amandine.m.lee@gmail.com>
2013-09-08 23:59:19 -0700

    Testing standard dictionary functions - nested dicts don't work

--- test_dict.py
+++ test_dict.py
@@ -13,24 +13,43 @@ class TestBigDict():
         return simpled
 
     @pytest.fixture
-    def dictdict(self):
+    def ddict(self):
         ddict = BigDict()
         ddict['banana'] = {}
         ddict['banana']['apple'] = 12
-
+        return ddict
 
     def test_simpled(self, simpled):
         assert simpled['a'] == 9
-        assert simpled[12] == 'b'
+        assert simpled[12] == 'a'
         simpled['a'] = 6
         assert simpled['a'] == 6
-        with raises(KeyError):
+        with pytest.raises(KeyError):
             b =  simpled['h']
 
     def test_ddict(self, ddict):
         assert ddict['banana']['apple'] == 12
-        with raises(KeyError):
-            whatever = ddict['apple']['potato']
-
-        
+        with pytest.raises(KeyError):
+            ddict['apple']['potato']
     
+    def test_len(self, simpled):
+        assert len(BigDict()) == 0
+        assert len(simpled) == 2
+        simpled['c'] = 13
+        assert len(simpled) == 3
+
+    def test_keys(self, simpled):
+        assert BigDict().keys() == []
+        assert simpled.keys().sort() == ['a',12].sort()
+        simpled[('thing', 'thang')] = -5
+        assert simpled.keys().sort() == [12, 'a', ('thing', 'thang')].sort()
+    
+    def test_values(self, simpled):
+        assert BigDict().values() == []
+        assert simpled.values().sort() == [9, 'a'].sort()
+        simpled['e'] = "hello"
+        assert simpled.values().sort() == [9, 'a', "hello"].sort()
+
+    def test_get(self, simpled):
+        assert simpled.get('a') == 9
+
